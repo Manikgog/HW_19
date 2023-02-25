0c111893cbf609c0a1d090dc9558cec70d6dbf04
Maxim Gogolin <maxinata@rambler.ru>
2023-02-25 17:03:25 +0500

    Добавлена функция find_and_sort.

--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@
 
 int gcd(int, int);
 int mirror_number(int);
+int r_mirror_number(int);
 int pow(int, int);
 int count_dig(int);
+void fill_arr(int* arr, const int size, int low, int hi);
+void print_arr(int* arr, const int size);
+int find_and_sort(int* arr, const int length, int value);
 
 int main() {
 	setlocale(LC_ALL, "Russian");
@@ -30,13 +34,80 @@ int main() {
 		Write a mirror_number function that
 	takes a number and returns it in a reflected form.
 	*/
-	int num;
+	/*int num;
 	std::cout << "Exercise 2.\nProgram for return number in a reflected form.\n";
 	std::cin >> num;
-	std::cout << mirror_number(num);
-	
+	std::cout << mirror_number(num);*/
+
+	// Exercise 3.
+	/*
+		Создайте функцию, которая принимает массив, его
+	длину и число N. Функция возвращает первую позицию
+	числа N в массиве, а также сортирует все элементы,
+	которые находятся справа от N.
+	*/
+	const int length = 10;
+	int arr[length];
+	fill_arr(arr, length, 0, 10);
+	std::cout << "Array:\n";
+	print_arr(arr, length);
+	int val;
+	std::cin >> val;
+	find_and_sort(arr, length, val);
+	print_arr(arr, length);
 	return 0;
 }
+
+int find_and_sort(int* arr, const int length, int value) {
+	int position = -1;
+	for (size_t i = 0; i < length; i++)
+	{
+		if (arr[i] == value) {
+			position = i;
+			break;
+		}
+	}
+	if (position != -1) {
+		for (size_t i = position+1; i < length; i++) {
+			for (size_t j = i; j > position; j--)
+			{
+				if (arr[j - 1] > arr[j]) {
+					int tmp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = tmp;
+				}
+			}
+		}
+	}
+	return position;
+}
+
+void print_arr(int* arr, const int size) {
+	std::cout << "[ ";
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << ", ";
+	}
+	std::cout << "\b\b ]\n";
+}
+
+void fill_arr(int* arr, const int size, int low, int hi) {
+	for (int i = 0; i < size; ++i) {
+
+		arr[i] = rand() % (hi + 1 - low) + low;
+
+	}
+}
+
+
+int r_mirror_number(int n) {
+	int digit_counter = count_dig(n);
+	digit_counter--;
+	int p = pow(10, digit_counter);
+	if (n <= 10)
+		return n;
+	return r_mirror_number(n / 10) * p + r_mirror_number(n / 10) * p;
+}
+
 
 // function for counting the number of digits of a number
 int count_dig(int n) {
